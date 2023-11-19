aef3a79753cb74441215e43c169797d1f687549e
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-11-19 11:42:20 +0900

    d

--- jetson/main.cpp
+++ jetson/main.cpp
@@ -370,10 +370,12 @@ int main(int argc, const char * argv[]) {
                         nodes[pos].dist_mm_q2/4.0f,
                         nodes[pos].quality >> SL_LIDAR_RESP_MEASUREMENT_QUALITY_SHIFT);
                 #endif
-                
-                printf("%s, %s, %s\n",typeid(nodes[pos].angle_z_q14).name(), typeid(nodes[pos].dist_mm_q2).name(), typeid(nodes[pos].quality).name());
 
-                //printf("%f, %f, %d\n", (nodes[pos].angle_z_q14 * 90.f) / 16384.f, nodes[pos].dist_mm_q2, nodes[pos].quality >> SL_LIDAR_RESP_MEASUREMENT_QUALITY_SHIFT);
+                float angle_in_degrees = nodes[pos].angle_z_q14 * 90.f / (1 << 14);
+                float distance_in_meters = nodes[pos].dist_mm_q2 / 1000.f / (1 << 2);
+                int quality = (int)nodes[pos].quality;
+
+                printf("%lf, %lf, %d \n", angle_in_degrees, distance_in_meters, quality);
 
                 /*測距データの提供を命令されていたら送信*/
                 if(send_data == 1){
