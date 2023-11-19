3e0070c30a78c64976370739ee1c75872cc683dd
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-11-19 13:49:27 +0900

    d

--- raspi/lidar.py
+++ raspi/lidar.py
@@ -21,11 +21,16 @@ def convertdata(received_data):
         if received_data[i] == 255 and received_data[i+9] == 254:
             if received_data[i+1] == 11:
                 for ii in range(3):
-                    out[idx,0] += received_data[i+2+ii] * np.power(254,i) / 1000.0
-                    out[idx,1] += received_data[i+5+ii] * np.power(254,i)
+                    out[idx,0] += received_data[i+2+ii] * np.power(254,ii) / 1000.0
+                    out[idx,1] += received_data[i+5+ii] * np.power(254,ii)
                 out[idx,2] = received_data[i+8]
                 
                 idx += 1
     
     return out[:idx]
                 
+def disconnect():
+    s.send([255,9,254])
+    time.sleep(0.25)
+    s.server.close()
+    
