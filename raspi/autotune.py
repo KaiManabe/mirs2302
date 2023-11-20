1f7a5af0a3d13e075cfc8fa1b7258a60003bb597
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-11-21 01:18:32 +0900

    pa

--- raspi/autotune.py
+++ raspi/autotune.py
@@ -14,8 +14,8 @@ STEP = [0.1, 0.01, 0.01]
 SPEED = 500
 RECTIME = 5
 LR_P = pow(10, -4)
-LR_I = pow(10, -5) * 2
-LR_D = pow(10, -5)
+LR_I = pow(10, -5)
+LR_D = pow(10, -6)
 
 
 
@@ -61,7 +61,7 @@ def setgain_arr(s, gain_arr):
             tuning.setgain(s, "R", pid[i], gain_arr[1][i])
 
 
-def grad(s, p, i, d, epsilonp = 0.05, epsiloni = 0.00035, epsilond = 0.0002):
+def grad(s, p, i, d, epsilonp = 0.1, epsiloni = 0.01, epsilond = 0.01):
     tuning.setgain(s, "L", "P", p)
     tuning.setgain(s, "R", "P", p)
     tuning.setgain(s, "L", "I", i)
