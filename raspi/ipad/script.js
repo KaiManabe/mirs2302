83d1fae4fdfd44d848bcb3edf22b872439c99d14
yamariku1126 <yamamotoriku1126@icloud.com>
2023-11-24 18:29:01 +0900

    コメントを追加

--- raspi/ipad/script.js
+++ raspi/ipad/script.js
@@ -1,24 +1,26 @@
-function performJudgment() {
-    // 入力データを取得
-    var inputData = document.getElementById("inputData").value;
+function performJudgment(inputData) {
+    // 入力データを取得（getElementById()：任意のHTMLタグで指定したIDにマッチするドキュメント要素を取得するメソッド）
+    // htmlがjsに渡すのではなく、jsがhtmlを読み込んでいる
+    //var inputData = document.getElementById("inputData").value; // document.：文字コードがUTF-8のHTMLのソースコード
 
     // XMLHttpRequestオブジェクトを使用してPHPにHTTPリクエストを送信
     var xhr = new XMLHttpRequest();
     var url = "perform_judgment.php";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.open("POST", url, true); // 第一引数：メソッド　第二引数：接続先のurl 第三引数：同期か非同期かを指定(Trueは非同期)
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // 第一引数：ヘッダ名　第二引数：ヘッダの値
 
     // 判定するデータを送信
-    var data = "inputData=" + encodeURIComponent(inputData);
+    var data = "inputData=" + encodeURIComponent(inputData); // encodeURIComponent()：与えられた文字列を表す URI 構成要素としてエンコードされた新しい文字列を返す
 
     // レスポンスが帰ってきた際の処理
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             // PHPから受け取った判定結果を表示
+            // id="result"の要素に返ってきた値を書き込む
             document.getElementById("result").innerHTML = xhr.responseText;
         }
     };
 
-    // データを送信
+    // エンコードされたデータを送信
     xhr.send(data);
 }
