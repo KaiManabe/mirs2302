d7f1ccb235b78c3017ed35d336387466e34ce768
yamariku1126 <yamamotoriku1126@icloud.com>
2023-11-24 16:11:54 +0900

    Webアプリテストプログラムの更新

--- raspi/ipad/get_state.js
+++ raspi/ipad/get_state.js
@@ -2,9 +2,9 @@ function sendData() {
     var time = document.getElementById("deliveryTime").value;
     var location = document.getElementById("deliveryLocation").value;
 
-    // AJAX request to send data to PHP
+    // phpにhttpリクエストを送る
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "process.php", true);
+    xhr.open("POST", "receive.php", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send("time=" + time + "&location=" + location);
 }
