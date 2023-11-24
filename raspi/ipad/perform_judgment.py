ae317a8a424aead3662e3b748a9b26373c36a94b
yamariku1126 <yamamotoriku1126@icloud.com>
2023-11-24 18:34:23 +0900

    テスト用の.pyを更新

--- raspi/ipad/perform_judgment.py
+++ raspi/ipad/perform_judgment.py
@@ -4,10 +4,6 @@ import sys
 input_data = sys.argv[1]
 
 # ここで必要な判定の処理を行う（サンプルとして文字列の長さを判定しています）
-if len(input_data) > 5:
-    result = "入力データは5文字より長いです。"
-else:
-    result = "入力データは5文字以下です。"
 
 # 判定結果を表示
-print(result)
+print(len(input_data))
