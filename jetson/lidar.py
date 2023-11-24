4a97e92a1ade92ef4277c3111de15902b4c00b75
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-11-24 19:18:16 +0900

    jetsonのlidar用プログラムを作り始めた

---
+++ jetson/lidar.py
@@ -0,0 +1,81 @@
+import sock
+import matplotlib.pyplot as plt
+import config
+import subprocess as sp
+import sys
+import time
+import numpy as np
+
+
+def getdata():
+    s.read()
+    s.send([255,3,254])
+    while(1):
+        if s.buffer_length() > 10:
+            break
+        time.sleep(0.1)
+    
+    return s.read()
+
+def convertdata(received_data):
+    out = np.zeros([int(len(received_data) / 10), 3])
+    idx = 0
+    for i in range(len(received_data)):
+        if (i+9) >= len(received_data):
+            break
+        if received_data[i] == 255 and received_data[i+9] == 254:
+            if received_data[i+1] == 11:
+                for ii in range(3):
+                    out[idx,0] += received_data[i+2+ii] * np.power(254,ii) / 1000.0
+                    out[idx,1] += received_data[i+5+ii] * np.power(254,ii)
+                out[idx,2] = received_data[i+8]
+                
+                idx += 1
+    
+    return out[:idx]
+                
+def disconnect():
+    s.send([255,9,254])
+    time.sleep(0.25)
+    s.server.close()
+
+
+def plotter(nparr):
+    xy = np.zeros([len(nparr), 2])
+    for i in range(len(nparr)):
+        xy[i,0] = -1 * nparr[i,1] * np.cos(nparr[i,0] / 180.0 * np.pi)
+        xy[i,1] = nparr[i,1] * np.sin(nparr[i,0] / 180.0 * np.pi)
+    
+    plt.scatter(xy[:,0], xy[:,1])
+    plt.scatter([0],[0], c = "red", marker = "x")
+    plt.show()
+
+def p():
+    received_data = []
+    while(1):
+        received_data = getdata()
+        if len(received_data) > 10:
+            break
+        time.sleep(0.5)
+    nparr = convertdata(received_data)
+    plotter(nparr)
+
+if __name__ == "__main__":
+    print("[INFO][lidar.py] : jetsonのデーモンプロセスをキルしています...")
+    sp.run(["sh", "/home/mirs2302/git/mirs2302/jetson/terminate_screen.sh"])
+    time.sleep(1)
+    s = sock.sock_server("127.0.0.1", config.CPP_PYTHON_SOCKET_PORT)
+    while(1):
+        if(s.server_started == True):
+            break
+    
+    print("[INFO][lidar.py] : ultra_simpleとのソケット通信を確立しています...")
+    sp.run(["/home/mirs2302/git/mirs2302/jetson/start_lidar.sh"])
+    
+    while(1):
+        if (s.isconnected() > 0):
+            break
+    
+    time.sleep(1)
+    
+    print(convertdata(getdata()))
