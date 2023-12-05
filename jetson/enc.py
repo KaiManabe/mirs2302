6c6edfe66747d36617727d59870428f15cd07652
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-12-06 02:11:45 +0900

    enc修正

--- jetson/enc.py
+++ jetson/enc.py
@@ -1,10 +1,20 @@
 import Jetson.GPIO as g
 import threading
+import time
+
 
 class enc():
-    def __init__(self):
+    def __init__(self, dt = 20):
+        """
+        jetsonのGPIOからエンコーダ値を監視するクラス
+        
+        引数：
+            dt(任意) : 速度を求めるときのΔt
+        """
         g.setmode(g.BOARD)
 
+        self.dt = dt
+        self.ctime = time.time()
         self.PINASSIGN = {"LA" : 35, "LB" : 36, "RA" : 32, "RB" : 33}
         for label in self.PINASSIGN:
             g.setup(self.PINASSIGN[label], g.IN)
@@ -13,21 +23,38 @@ class enc():
         self.prev = {"LA" : 0, "LB" : 0, "RA" : 0, "RB" : 0}
         self.current = {"LA" : 0, "LB" : 0, "RA" : 0, "RB" : 0}
         self.pulse_L = 0
+        self.pulse_L_prev = 0
+        self.pulse_L_d = 0
         self.pulse_R = 0
+        self.pulse_R_prev = 0
+        self.pulse_R_d = 0
     
     def start_monitor(self):
         t = threading.Thread(target = monitor, args = (self,))
         t.setDaemon(True)
         t.start()
+        
+        
+        t2 = threading.Thread(target = verocity_monitor, args = (self,))
+        t2.setDaemon(True)
+        t2.start()
     
     def reset(self):
         self.pulse_L = 0
+        self.pulse_L_prev = 0
+        self.pulse_L_d = 0
         self.pulse_R = 0
+        self.pulse_R_prev = 0
+        self.pulse_R_d = 0
 
-    def get(self):
-        return self.pulse_L, self.pulse_R
 
-
+def verocity_monitor(e:enc):
+    while(1):
+        if time.time() >= e.ctime + (e.dt / 1000):
+            e.pulse_L_d = e.pulse_L - e.pulse_L_prev
+            e.pulse_R_d = e.pulse_R - e.pulse_R_prev
+            e.pulse_L_prev = e.pulse_L
+            e.pulse_R_prev = e.pulse_R
     
 def monitor(e:enc):
     while(1):
