b8ab069b145bf35dca4f021fa274a8003dbcab21
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-12-06 14:03:13 +0900

    main/の更新

--- raspi/html/main/view.js
+++ raspi/html/main/view.js
@@ -4,7 +4,7 @@
 表示するhtmlファイルを変更する関数
 引数：表示したいhtmlファイルのiframe要素のid(文字列)
 */
-function select_view_html(id){
+function select_view_html(iframe_id){
     // 全てのiframe要素のclass名"view"を消去し、"hide"を追記
 	var element = document.querySelectorAll("iframe");
 	for(let i = 0; i < element.length; i++){
@@ -13,6 +13,6 @@ function select_view_html(id){
     }
 
     // 選択されたidのclass名"hide"を"view"に書き換え
-    selected = document.querySelector(`#${id}`);
+    selected = document.querySelector(`#${iframe_id}`);
 	selected.classList.replace("hide" , "view");
 }
