e934cb4931d659b5127a66778de17e1296c200b1
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-12-08 19:03:25 +0900

    phpにデータを送れるようにした(多分完成)

--- raspi/html/send/get_available_selection.js
+++ raspi/html/send/get_available_selection.js
@@ -2,51 +2,56 @@
 全体を実行するやつ
 */
 function execution(){
-    data = receiveFormData();
-    sendData(data);
+    sendData = readFormData();
+    receiveData = exchangeDataPhp(sendData);
 }
 
 /*
-formデータを受け取り、データを送信する関数
-引数・戻り値なし
+formデータ読み込む関数
+
+引数：なし
+戻り値：formデータの配列
 */
-function receiveFormData(){
+function readFormData(){
     var inputIds = ['client_address', 'client_address_type', 'target_address', 'target_address_type', 'item_type', 'item_name', 'picking_place', 'picking_time', 'picking_pincode', 'note'];
-    var data = {};
+    var formData = {};
 
     // 各IDの入力フォームの値をdataに格納
     inputIds.forEach(function(id) {
-        data[id] = document.getElementById(id).value;
+        formData[id] = document.getElementById(id).value;
     });
 
-    return data;
+    return formData;
 }
 
 /*
-データを送信し結果を返す関数
+phpとやり取りする関数
 
-引数：送りたいデータ(配列も可能)
-戻り値：結果(にしたい)
+引数：phpに送りたいデータ(json形式で送信する)
+戻り値：結果
 */
-function sendData(inputData) {
+function exchangeDataPhp(sendData) {
     // XMLHttpRequestオブジェクトを使用してPHPにHTTPリクエストを送信
     var xhr = new XMLHttpRequest();
     var url = "receive.php";
     
-    xhr.open("POST", url, false); // 第一引数：メソッド　第二引数：接続先のurl 第三引数：同期(true)か非同期(false)かを指定
+    xhr.open("POST", url, false); // 第一引数：メソッド　第二引数：接続先のurl 第三引数：同期(false)か非同期(true)かを指定
     xhr.setRequestHeader("Content-Type", "application/json"); // ヘッダを設定(文字列も送れるjson形式を指定)
-    xhr.send(JSON.stringify(inputData)); // データを送信
+    xhr.send(JSON.stringify(sendData)); // データを送信
 
-    // レスポンスが帰ってきた際の処理
-    xhr.onreadystatechange = function () {
-        // xhr.readyState == 4 は通信完了、xhr.status == 200 は通信成功を表す
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log(xhr.responseText); //　開発者デバッグ用
-            document.getElementById("result").innerHTML = xhr.responseText; // id="result"の要素に返ってきた値を書き込む
+    return xhr.responseText;
+}
 
-            // 戻り値を結果にしようとしてるけどうまくいかない
-            // result = xhr.responseText;
-            // return result;
-        }
-    };
+/*
+デバッグ用関数（うまく動かん）
+
+引数：htmlに追加したいデータ（配列）
+戻り値：なし
+*/
+function addHTML(data){
+    var exdata = data;
+    // 配列の各要素をliタグで囲んでinnerHTMLに挿入
+    document.getElementById("result").innerHTML = "<ul>" + exdata.map(function(item) {
+        return "<li>" + item + "</li>";
+    }).join("") + "</ul>";  // 配列を文字列に結合し、ulタグで囲む
 }
--- raspi/html/send/receive.php
+++ raspi/html/send/receive.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // 受け取ったデータをJSONから連想配列に変換
     $receivedData = json_decode(file_get_contents('php://input'), true);
 
-    // 受け取ったデータを処理する（echoはhttpレスポンスとして返す）
+    // とりあえず受け取ったデータをそのままhttpレスポンスとして返す
     echo json_encode(['status' => 'success', 'data' => $receivedData]);
 } else {
     // POSTリクエスト以外はエラーとする
