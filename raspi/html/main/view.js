528ba906bcfe922551a6cc5148c6e94ca02665cf
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-12-26 20:49:45 +0900

    Webページのスタイル変更

--- raspi/html/main/view.js
+++ raspi/html/main/view.js
@@ -6,13 +6,30 @@
 */
 function select_view_html(iframe_id){
     // 全てのiframe要素のclass名"view"を消去し、"hide"を追記
-	var element = document.querySelectorAll("iframe");
-	for(let i = 0; i < element.length; i++){
-		element[i].classList.remove("view");
-        element[i].classList.add("hide");
+	var iframeElement = document.querySelectorAll("iframe");
+	for(let i = 0; i < iframeElement.length; i++){
+		iframeElement[i].classList.remove("view");
+        iframeElement[i].classList.add("hide");
     }
 
     // 選択されたidのclass名"hide"を"view"に書き換え
     selected = document.querySelector(`#${iframe_id}`);
 	selected.classList.replace("hide" , "view");
 }
+
+
+// ナビゲーションメニューのリストアイテムを取得
+var navItems = document.querySelectorAll('nav ul li div');
+// 各リストアイテムにクリックイベントを追加
+navItems.forEach(function(item) {
+  item.addEventListener('click', function() {
+    // すべてのリストアイテムからクラスを削除
+    navItems.forEach(function(item) {
+      item.classList.remove('selected');
+    });
+
+    // クリックされたリストアイテムにクラスを追加
+    item.classList.add('selected');
+  });
+});
+
