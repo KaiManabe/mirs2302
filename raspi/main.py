1ae989f1e1a44c7a206872c01d892852a67a773b
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-12-28 00:15:52 +0900

    mainつくりはじめ

--- raspi/main.py
+++ raspi/main.py
@@ -11,7 +11,7 @@ import datetime
 import time
 import sys
 
-
+#デバッグ用
 def set_goal():
      for i in range(3):
         idx = 2 - i
@@ -58,6 +58,157 @@ if __name__ == "__main__":
     #オーダー管理用クラス
     order_manager = order_mng.order_manager()
     
-    time.sleep(5)
+#モジュール管理用云々を初期化しておくこと
     
-   
+    
+    print(f"[INFO][main.py] : 初期化終了 メインループを開始します")
+    
+    #1ループの処理時間は短めにすること
+    while(1):
+        for i in range(len(order_manager.df)):
+            current_order = order_manager.get_order(i)
+            id = current_order["ID"]
+            status = current_order["STATUS"]
+            
+#*****************************************************************************************************
+#状態    NOT_ACCEPTED_YET
+#*****************************************************************************************************
+            if status == "NOT_ACCEPTED_YET":
+#メールを送る処理
+                order_manager.modify_order(id, "STAUTS", "MAIL_SENT")
+            
+            
+#*****************************************************************************************************
+#状態    MAIL_SENT
+#*****************************************************************************************************        
+            elif status == "MAIL_SENT":
+#条件文↓
+                if """メールのレスポンスがあったか？""":
+#条件文↓
+                    if """承認されたか？""":
+                        order_manager.modify_order(id, "STAUTS", "ACCEPTED")
+                        order_manager.modify_order(id, "ACCEPTED_TIME", datetime.datetime.now())
+#承認されましたメール送信
+                    else:#拒否された
+                        order_manager.modify_order(id, "STAUTS", "DENIED")
+#拒否されましたメール送信
+                else:#レスポンスなし
+#条件文↓
+                    if """タイムアウトしてるか？""":
+                        order_manager.modify_order(id, "STAUTS", "ACCEPT_TIMEOUT")
+#タイムアウトしたので拒否しますメール送信
+            
+            
+            
+#*****************************************************************************************************
+#状態    ACCEPTED
+#*****************************************************************************************************
+            elif status == "ACCEPTED":
+                #今のところ10分前に移動させるようにしているが調整するならどうにかする
+                if datetime.datetime.now() >= current_order["PICKUP_TIME"] - datetime.timedelta(minutes = 10):
+                    #ゴール設定
+                    ros_controller.set_goal(current_order["PICKUP_PLACE"])
+                    
+                    #走り出すまで待機
+                    time.sleep(0.5)
+                    for i in range(3):
+                        if ros_controller.status == "ACTIVE":
+                            print(f"[INFO][main.py][ID:{id}] : ロボットが走行中です...")
+                            order_manager.modify_order(id, "STAUTS", "MOVING_FOR_PICKUP")
+#今向かってますてきなメールあれば◎  やらないなら無視
+                            break
+                        else:
+                            time.sleep(5)
+                    if i == 2:
+                        print(f"[FATAL][main.py][ID:{id}]  : 移動を開始できませんでした ROSステータス : {ros_controller.status}", file = sys.stderr)
+                    
+                    
+            
+#*****************************************************************************************************
+#状態    MOVING_FOR_PICKUP
+#*****************************************************************************************************                
+            elif status == "MOVING_FOR_PICKUP":
+                if ros_controller.stauts == "SUCCEEDED":
+                    print(f"[INFO][main.py][ID:{id}]  : ロボットが{current_order['PICKUP_PLACE']}に到着")
+                    order_manager.modify_order(id, "STAUTS", "WAITING_FOR_PICKUP")
+#到着しましたてきなメールあれば◎  やらないなら無視
+                else:
+                    if ros_controller.stauts == "ACTIVE":#まだ動いているなら
+                        if datetime.datetime.now() >= current_order["PICKUP_TIME"] + datetime.timedelta(minutes = 5):
+                            print(f"[FATAL][main.py][ID:{id}]  : ロボットがスタックしている可能性があります ROSステータス : {ros_controller.status}", file = sys.stderr)
+#スタックしてる場合にバックさせる
+                    else:#動いてないし到着してない
+                        print(f"[FATAL][main.py][ID:{id}]  : ロボットが到着できませんでした リトライします ROSステータス : {ros_controller.status}", file = sys.stderr)
+                        order_manager.modify_order(id, "STAUTS", "ACCEPTED")
+            
+            
+#*****************************************************************************************************
+#状態    WAITING_FOR_PICKUP
+#***************************************************************************************************** 
+            elif status == "WAITING_FOR_PICKUP":
+#条件文↓
+                if """ユーザきたか？""":
+                    order_manager.modify_order(id, "STAUTS", "PICKED_UP")
+                else:#きてない
+                    if datetime.datetime.now() >= current_order["PICKUP_TIME"] + datetime.timedelta(minutes = 15):
+                        order_manager.modify_order(id, "STAUTS", "PICKUP_TIMEOUT")
+#タイムアウトしたので拒否しますメール送信
+            
+            
+#*****************************************************************************************************
+#状態    PICKED_UP
+#***************************************************************************************************** 
+            elif status == "PICKED_UP":
+#ラボ前に帰る?　予約がどれだけの頻度で入っているかによる
+                #今のところ10分前に移動させるようにしているが調整するならどうにかする
+                if datetime.datetime.now() >= current_order["RECEIVE_TIME"] - datetime.timedelta(minutes = 10):
+                    #ゴール設定
+                    ros_controller.set_goal(current_order["RECEIVE_PLACE"])
+                    
+                    #走り出すまで待機
+                    time.sleep(0.5)
+                    for i in range(3):
+                        if ros_controller.status == "ACTIVE":
+                            print(f"[INFO][main.py][ID:{id}] : ロボットが走行中です...")
+                            order_manager.modify_order(id, "STAUTS", "MOVING_FOR_PICKUP")
+#今向かってますてきなメールあれば◎  やらないなら無視
+                            break
+                        else:
+                            time.sleep(5)
+                    if i == 2:
+                        print(f"[FATAL][main.py][ID:{id}]  : 移動を開始できませんでした ROSステータス : {ros_controller.status}", file = sys.stderr)
+
+
+
+            
+#*****************************************************************************************************
+#状態    MOVING_FOR_RECEIVE
+#*****************************************************************************************************                
+            elif status == "MOVING_FOR_RECEIVE":
+                if ros_controller.stauts == "SUCCEEDED":
+                    print(f"[INFO][main.py][ID:{id}]  : ロボットが{current_order['RECEIVE_PLACE']}に到着")
+                    order_manager.modify_order(id, "STAUTS", "WAITING_FOR_RECEIVE")
+#到着しましたてきなメールあれば◎  やらないなら無視
+                else:
+                    if ros_controller.stauts == "ACTIVE":#まだ動いているなら
+                        if datetime.datetime.now() >= current_order["RECEIVE_TIME"] + datetime.timedelta(minutes = 5):
+                            print(f"[FATAL][main.py][ID:{id}]  : ロボットがスタックしている可能性があります ROSステータス : {ros_controller.status}", file = sys.stderr)
+#スタックしてる場合にバックさせる
+                    else:#動いてないし到着してない
+                        print(f"[FATAL][main.py][ID:{id}]  : ロボットが到着できませんでした リトライします ROSステータス : {ros_controller.status}", file = sys.stderr)
+                        order_manager.modify_order(id, "STAUTS", "PICKED_UP")
+            
+            
+            
+#*****************************************************************************************************
+#状態    WAITING_FOR_RECEIVE
+#***************************************************************************************************** 
+            elif status == "WAITING_FOR_RECEIVE":
+#条件文↓
+                if """ユーザきたか？""":
+                    order_manager.modify_order(id, "STAUTS", "RECEIVED")
+                else:#きてない
+                    if datetime.datetime.now() >= current_order["RECEIVE_TIME"] + datetime.timedelta(minutes = 15):
+                        order_manager.modify_order(id, "STAUTS", "RECEIVE_TIMEOUT")
+#タイムアウトしたので拒否しますメール送信
+
--- raspi/raspi_to_ros.py
+++ raspi/raspi_to_ros.py
@@ -16,6 +16,12 @@ STATUS = [
     "LOST"]
 
 
+#ゴールのindexをjetsonと合わせる
+GOALS = {"D科棟" : 0,
+         "学生係" : 1
+         }
+
+
 
 class raspi_to_ros():
     def __init__(self, serv:sock.sock_server):
@@ -29,10 +35,13 @@ class raspi_to_ros():
         t.setDaemon(True)
         t.start()
         
-    def set_goal(self, goal_index:int):
+    def set_goal(self, goal:str):
+        if not(goal in GOALS.keys()):
+            print("[ERR][raspi_to_ros] : set_goalの引数が不正です", file = sys.stderr)
+            return -1
         if self.serv.isconnected() > 0:
-            return self.serv.send([255,goal_index,254])
-            
+            print(f"[INFO][raspi_to_ros] : 目的地を{goal}に設定しました")
+            return self.serv.send([255,GOALS[goal],254])
         else:
             print("[ERR][raspi_to_ros] : ros側のクライアントサーバが起動していません", file = sys.stderr)
             return -1
@@ -43,3 +52,19 @@ class raspi_to_ros():
                 st = self.serv.read()
                 self.status = STATUS[st[-1]]
             time.sleep(1)
+    
+    
+    def movement_monitor(self):
+        while(1):
+            time.sleep(0.2)
+            if self.status == "ACTIVE":
+                break
+        print(f"[INFO][raspi_to_ros] : ロボットが走行中です... ROSステータス : {self.status}")
+        while(1):
+            time.sleep(0.2)
+            if self.status == "SUCCEEDED":
+                print(f"[INFO][raspi_to_ros] : 目的地{idx}に到着しました ROSステータス : {self.status}")
+                break
+            elif self.status != "ACTIVE":
+                print(f"[INFO][raspi_to_ros] : 目的地{idx}に到着できませんでした ROSステータス : {self.status}")
+                sys.exit(1)
