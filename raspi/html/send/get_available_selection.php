3111773a9085b5eacf253401ca52efef33bc3dcf
Kai Manabe <d20136@numazu.kosen-ac.jp>
2023-12-27 15:25:44 +0900

    変更後のphpのみ一旦

--- raspi/html/send/get_available_selection.php
+++ raspi/html/send/get_available_selection.php
@@ -1,14 +1,15 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET'){
     if(isset($_GET['time'])){
-        $pythonScriptPath = "/home/pi/git/mirs2302/raspi/available_picking_place.py";
+        $type = 'time'; // 引数の値のタイプ
         $receivedData = json_decode($_GET['time']); // GETメソッドのクエリ文字列のtimeデータを取得
-    } elseif(isset($_GET['place'])){
-        $pythonScriptPath = "/home/pi/git/mirs2302/raspi/available_picking_time.py";
-        $receivedData = json_decode($_GET['place']); // GETメソッドのクエリ文字列のplaceデータを取得
+    } elseif(isset($_GET['item_type'])){
+        $type = 'type'; // 引数の値のタイプ
+        $receivedData = json_decode($_GET['item_type']); // GETメソッドのクエリ文字列のplaceデータを取得
     }
 
     // pythonスクリプトを実行
+    $pythonScriptPath = "/home/pi/git/mirs2302/raspi/available.py";
     $command = "python3 $pythonScriptPath $receivedData"; // なぜかescapeshellarg()を使用すると2byte文字が消えてしまう
     $result = shell_exec($command);
     
