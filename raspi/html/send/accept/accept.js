f58a5516d9e6b24749f357cb39d8f725fed3dfe7
Kai Manabe <d20136@numazu.kosen-ac.jp>
2024-01-03 14:59:10 +0900

    acceptでフォームが空でも送信できるバグ修正

--- raspi/html/send/accept/accept.js
+++ raspi/html/send/accept/accept.js
@@ -42,59 +42,6 @@ function onLoad(){
 
 }
 
-/*
-選択可能な時間を制限する関数
-
-引数(なしでもいける)：
-    場所: str
-
-***クライアントが集荷場所を選択した時に実行するやつ***
-*/
-function selectableTime(place) {
-    // httpリクエストを送信して選択可能な時間を取得
-    var xhr = new XMLHttpRequest();
-    var url = "get_available_selection.php"; // httpリクエスト先
-    xhr.open("GET", url + "?place=" + encodeURIComponent(JSON.stringify(place)), false); // 同期通信GETメソッド
-    xhr.send();
-    var timeList = xhr.responseText.split(",").filter(Boolean); // httpレスポンスを配列にして受け取る
-
-    // 集荷時間の要素を取得
-    var selectElement = document.getElementById("picking_time");
-
-    // 既存の選択肢の配列を作成
-    var existingOptions = timelist_available;
-
-    // 既存の選択肢にない選択可能な集荷時間を追加
-    timeList.forEach(function(time) {
-        if (!existingOptions.includes(time)) {
-            //var option = document.createElement("option");
-            option.value = time;
-            option.text = time;
-            //selectElement.appendChild(option);
-        }
-    });
-    // 既存の選択肢にある選択可能な集荷時間ではないものを削除
-    existingOptions.forEach(function(optionValue) {
-        if (!timeList.includes(optionValue) && optionValue != 'init') {
-            selectElement.querySelectorAll('option[value="' + optionValue + '"]').forEach(option => option.remove());
-        }
-    });
-
-    // 集荷時間の選択肢をソート
-    Array.from(selectElement.options)
-    .filter(option => option.value !== 'init')
-    .sort((a, b) => {
-        if (a.value < b.value) {
-            return -1;
-        }
-        if (a.value > b.value) {
-            return 1;
-        }
-        return 0;
-    })
-    .forEach(option => selectElement.appendChild(option));
-}
-
 
 /*
 選択可能な時間を制限する関数
@@ -181,74 +128,10 @@ function chbox(obj){
 }
 
 
-/*
-formデータ読み込む関数
 
-引数：
-    formのID
-戻り値：
-    formデータの配列
-*/
-function readFormData(formIds) {
-    var formData = {};
 
-    // 各IDの入力フォームの値をformDataに格納
-    formIds.forEach(function (id) {
-        formData[id] = document.getElementById(id).value;
-    });
-
-    let current_url = new URL(window.location.href);
-    let params = current_url.searchParams;
-    var order_id = params.get('id'); //order_idを取得
-
-    formData["id"] = order_id;
-    return formData;
-}
-
-/*
-送信ボタンに関する処理
-*/
-function submitProcessing(){
-    // formデータの読み込み
-    var formIds = ['accept', 'receive_pincode','receive_time', 'receive_place']; // formのID
-    var sendData = readFormData(formIds);
-
-    // formデータを送信
-    var result = sendDataToPhp(sendData);
-    return result
-    // 送信結果の画面を表示
-    var sendingElement = document.getElementById("sending");
-    // 正常終了
-    if(result == 0){
-        sendingElement.innerHTML = "<div id='success'>正常に取引を承りました</div>";
-    }
-    // 異常終了
-    else{
-        sendingElement.innerHTML = "<div id='error'>ERROR : 取引を承れませんでした</div>";
-    }
-}
-
-/*
-phpに送信する関数
-
-引数：
-    phpに送りたいデータ(json形式で送信する)
-*/
-function sendDataToPhp(sendData) {
-    // XMLHttpRequestオブジェクトを使用してPHPにHTTPリクエストを送信
-    var xhr = new XMLHttpRequest();
-    var url = "picking.php";
-    
-    xhr.open("POST", url, false); // 第一引数：メソッド　第二引数：接続先のurl 第三引数：同期(false)か非同期(true)かを指定
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); // ヘッダを設定(文字列も送れるjson形式を指定)
-    xhr.send(JSON.stringify(sendData)); // データを送信
-
-    return xhr.responseText;
-}
-
-//イベントリスナー
-const formElement = document.getElementById("form");
 const pickingTimeElement = document.getElementById("receive_time");
+const submit_button = document.getElementById("submit_button");
 
 // ページが読み込まれた時に実行する処理
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -260,8 +143,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
 pickingTimeElement.addEventListener('change', function(event) {
     selectableTime();
 });
+
+
+
 // データを送信するボタンが押された時の処理
-formElement.addEventListener('submit', function(event) {
-    event.preventDefault(); // ページのリロードを防ぐ
-    submitProcessing();
+submit_button.addEventListener("click", function(){
+    let form_ele = document.querySelector("#form_ele");
+    let select_ele = document.querySelectorAll('#form_ele select');
+    let input_ele = document.querySelectorAll('#form_ele input');
+    let isempty = false;
+
+    for (let i = 0; i < select_ele.length; i++){
+        if(select_ele[i].value == "init"){
+            isempty = true;
+        }
+    }
+
+    
+    for (let i = 0; i < input_ele.length; i++){
+        if(input_ele[i].value == ""){
+            isempty = true;
+        }
+    }
+
+    if(document.querySelector("#accept").value == "denied"){
+        isempty = false;
+    }
+
+    if(isempty){
+        window.alert("すべての必須項目を入力してください。");
+    }else{
+        form_ele.submit();
+    }
 });
