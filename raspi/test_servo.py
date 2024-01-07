23f2912d290ef35402c23554c0f79b09a39563fd
Kai Manabe <d20136@numazu.kosen-ac.jp>
2024-01-07 16:09:02 +0900

    サーボテスト用更新

--- raspi/test_servo.py
+++ raspi/test_servo.py
@@ -1,16 +1,32 @@
 import serial_com as ser
 import RPi.GPIO as GPIO
 import time
+import atexit
 
 s = ser.arduino_serial()
 
+PINS = [13,15,19,21,29,31]
+
 def servo(pin):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, True)
+    GPIO.output(pin, 0)
     time.sleep(0.5)
     s.send(10, [0])
     time.sleep(3)
     
-    GPIO.output(pin, False)
+    GPIO.output(pin, 1)
     time.sleep(0.5)
+    
+
+def init():
+    GPIO.setmode(GPIO.BOARD)
+    for pin in PINS:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin,1)
+
+def dele():
+    for pin in PINS:
+        GPIO.cleanup(pin)
+
+if __name__ == "__main__":
+    init()
+    atexit.register(dele)
