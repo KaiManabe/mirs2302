2c65fabfcb21cf0b0b8daa09aaa68ebb4d21554d
Kai Manabe <d20136@numazu.kosen-ac.jp>
2024-01-10 02:44:36 +0900

    modal修正

--- raspi/html/about/init.js
+++ raspi/html/about/init.js
@@ -2,6 +2,11 @@ function onload(){
     hide_modal("modal_loading");
 }
 
-//document.addEventListener("DOMContentLoaded", onload);
-document.querySelector("#main_pic").addEventListener('load', onload);
 show_modal("modal_loading");
+
+//document.addEventListener("DOMContentLoaded", onload);
+if(document.querySelector("#main_pic").complete){
+    onload();
+}else{
+    document.querySelector("#main_pic").addEventListener('load', onload);
+}
--- raspi/html/about/modal.js
+++ raspi/html/about/modal.js
@@ -1,7 +1,22 @@
+var timer_id;
+var body_ele;
+var sorry;
+
+sorry = document.createElement("p");
+sorry.innerHTML = "処理に時間がかかっています。<br>1分程度かかる場合があります。"
+sorry.setAttribute("style", "font-size: 0.75em;");
+
+function dispmsg(){
+    body_ele.appendChild(sorry);
+}
+
+
 function show_modal(modal_id){
+    window.clearTimeout(timer_id);
+    timer_id = window.setTimeout(dispmsg, 5000);
+
     back_ele = document.querySelector("#modal");
     back_ele.classList.remove("hidden");
-
     if(modal_id[0] == "#"){
         id = modal_id;
     }else{
@@ -23,4 +38,5 @@ function hide_modal(modal_id){
     }
     body_ele = document.querySelector(id);
     body_ele.classList.add("hidden");
+    body_ele.removeChild(sorry);
 }
